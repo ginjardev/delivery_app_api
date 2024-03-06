bd91aee6133dc7d59a7cd633d2664bb2948f41c3
ginjardev <arthurshur@gmail.com>
2024-03-06 11:11:07 +0000

    create order api

--- order_routes.py
+++ order_routes.py
@@ -1,10 +1,56 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
+from fastapi_jwt_auth import AuthJWT
+from models import User, Order
+from schemas import OrderModel
+from database import SessionLocal, engine
+from fastapi.exceptions import HTTPException
+from fastapi.encoders import jsonable_encoder
 
 order_route = APIRouter(
-	prefix='/order',
-	tags=['order']
+	prefix='/orders',
+	tags=['orders']
 )
 
+session = SessionLocal(bind=engine)
+
 @order_route.get('/')
-async def orders():
+async def order_home(Authorize:AuthJWT=Depends()):
+	try:
+		Authorize.jwt_required()
+	except Exception as e:
+		raise HTTPException(
+			status_code=status.HTTP_401_UNAUTHORIZED,
+			detail="Invalid token"
+		)
 	return {'Order List': 'Orders'}
+
+
+@order_route.post('/order', status_code=status.HTTP_201_CREATED)
+async def place_order(order:OrderModel, Authorize:AuthJWT=Depends()):
+	try:
+		Authorize.jwt_required()
+	except Exception as e:
+		raise HTTPException(
+			status_code=status.HTTP_401_UNAUTHORIZED,
+			detail="Invalid token"
+		)
+	
+	current_user=Authorize.get_jwt_subject()
+	user = session.query(User).filter(User.username==current_user).first()
+	new_order = Order(
+		pizza_size=order.pizza_size,
+		quantity=order.quantity
+	)
+	new_order.user = user
+	session.add(new_order)
+	session.commit()
+
+	response = {
+		"id": new_order.id,
+		"pizza_size": new_order.pizza_size,
+		"quantity": new_order.quantity,
+		"order status": new_order.order_status
+	}
+
+	return jsonable_encoder(response)
+ 
--- schemas.py
+++ schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from typing import Optional
 
+
 class SignUpModel(BaseModel):
     id: Optional[int]
     username: str
@@ -29,5 +30,22 @@ class Settings(BaseModel):
 
 
 class LoginModel(BaseModel):
-	username: str
-	password: str
+    username: str
+    password: str
+
+
+class OrderModel(BaseModel):
+    id: Optional[int]
+    quantity: int
+    order_status: Optional[str] = "PENDING"
+    pizza_size: Optional[str] = "SMALL"
+    user_id: Optional[int]
+
+    class Config:
+        orm_mode = True
+        schema_extra = {
+            "example": {
+                "quantity": 2, 
+                "pizza_size": "SMALL" 
+                }
+            }
