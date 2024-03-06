351c4b7348c0432ba63efc2a51d756c32c8cbbfe
ginjardev <arthurshur@gmail.com>
2024-03-06 15:20:36 +0000

    created user order route

--- order_routes.py
+++ order_routes.py
@@ -6,51 +6,101 @@ from database import SessionLocal, engine
 from fastapi.exceptions import HTTPException
 from fastapi.encoders import jsonable_encoder
 
-order_route = APIRouter(
-	prefix='/orders',
-	tags=['orders']
-)
+order_route = APIRouter(prefix="/orders", tags=["orders"])
 
 session = SessionLocal(bind=engine)
 
-@order_route.get('/')
-async def order_home(Authorize:AuthJWT=Depends()):
-	try:
-		Authorize.jwt_required()
-	except Exception as e:
-		raise HTTPException(
-			status_code=status.HTTP_401_UNAUTHORIZED,
-			detail="Invalid token"
-		)
-	return {'Order List': 'Orders'}
+
+@order_route.get("/")
+async def order_home(Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+        )
+    return {"Order List": "Orders"}
 
 
-@order_route.post('/order', status_code=status.HTTP_201_CREATED)
-async def place_order(order:OrderModel, Authorize:AuthJWT=Depends()):
-	try:
-		Authorize.jwt_required()
-	except Exception as e:
-		raise HTTPException(
-			status_code=status.HTTP_401_UNAUTHORIZED,
-			detail="Invalid token"
-		)
-	
-	current_user=Authorize.get_jwt_subject()
-	user = session.query(User).filter(User.username==current_user).first()
-	new_order = Order(
-		pizza_size=order.pizza_size,
-		quantity=order.quantity
+@order_route.post("/order", status_code=status.HTTP_201_CREATED)
+async def place_order(order: OrderModel, Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+        )
+
+    current_user = Authorize.get_jwt_subject()
+    user = session.query(User).filter(User.username == current_user).first()
+    new_order = Order(pizza_size=order.pizza_size, quantity=order.quantity)
+    new_order.user = user
+    session.add(new_order)
+    session.commit()
+
+    response = {
+        "id": new_order.id,
+        "pizza_size": new_order.pizza_size,
+        "quantity": new_order.quantity,
+        "order status": new_order.order_status,
+    }
+
+    return jsonable_encoder(response)
+
+
+@order_route.get("/orders")
+async def list_orders(Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+        )
+
+    current_user = Authorize.get_jwt_subject()
+
+    user = session.query(User).filter(User.username == current_user).first()
+    if user.is_staff:
+        orders = session.query(Order).all()
+        return jsonable_encoder(orders)
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, 
+        detail="You're not a superuser"
+    )
+
+
+@order_route.get('/orders/{id}')
+async def get_order_by_id(id:int, Authorize:AuthJWT=Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="invalid token"
+        )
+    current_user = Authorize.get_jwt_subject()
+    
+    user = session.query(User).filter(User.username==current_user).first()
+    if user.is_staff:
+        order = session.query(Order).filter(Order.id==id).first()
+        return jsonable_encoder(order)
+    
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detaail="You're not a superuser to fetch this data"
 	)
-	new_order.user = user
-	session.add(new_order)
-	session.commit()
 
-	response = {
-		"id": new_order.id,
-		"pizza_size": new_order.pizza_size,
-		"quantity": new_order.quantity,
-		"order status": new_order.order_status
-	}
 
-	return jsonable_encoder(response)
- 
+@order_route.get('/user/orders')
+async def get_user_orders(Authorize:AuthJWT=Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid token"
+        )
+    current_user = Authorize.get_jwt_subject()
+    
+    user = session.query(User).filter(User.username==current_user).first()
+    
+    return jsonable_encoder(user.orders)
